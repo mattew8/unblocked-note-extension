3e4bb9f42c0cada91e087066040b1a2a76d2929a
mattew8 <mattew4483@likelion.org>
2024-06-14 14:58:31 +0900

    ✅ open side panel in popup

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,5 +1,21 @@
+import { openSidePanelController } from '../service/message-controller/open-side-panel';
+
 const Popup = () => {
-  return <div>popup</div>;
+  const onClickOpen = async () => {
+    try {
+      await openSidePanelController.send();
+      window.close();
+    } catch (e) {
+      alert(e || 'fail to load side-panel. please retry later.');
+    }
+  };
+
+  return (
+    <div style={{ width: 320, height: 210, textAlign: 'center' }}>
+      <p>let's start unblocked note</p>
+      <button onClick={onClickOpen}>open</button>
+    </div>
+  );
 };
 
 export default Popup;
