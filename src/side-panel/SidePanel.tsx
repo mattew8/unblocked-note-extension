e4660f7c54782af733001047ce24946044311522
mattew8 <mattew4483@likelion.org>
2024-06-16 22:50:52 +0900

    ✅ add PostCreate

--- src/side-panel/SidePanel.tsx
+++ src/side-panel/SidePanel.tsx
@@ -1,4 +1,5 @@
 import { useExtensionRouter } from '../service/router/extension-router';
+import PostCreate from './post-create/PostCreate';
 import PostDetail from './post-detail/PostDetail';
 import PostList from './post-list/PostList';
 
@@ -9,6 +10,7 @@ const SidePanel = () => {
   return (
     <>
       {pathname === 'post/list' && <PostList />}
+      {pathname === 'post/create' && <PostCreate />}
       {pathname === 'post/detail' && postId !== null && (
         <PostDetail id={Number(postId)} />
       )}
