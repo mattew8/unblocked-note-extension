f6645425d71a0aec13eeb7d990d21ffedffbff55
mattew8 <mattew4483@likelion.org>
2024-06-16 21:33:12 +0900

    ✅ post list, detail component

---
+++ src/side-panel/post-detail/PostDetail.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { postService } from '../../service/post/post-service';
+
+const EDITABLE_ELEMENT_ID = 'editable-div';
+
+interface PostDetailProps {
+  id: string;
+}
+const PostDetail = ({ id }: PostDetailProps) => {
+  useEffect(() => {
+    console.log(id);
+  }, [id]);
+
+  const saveNotes = () => {
+    const editText = document.getElementById(EDITABLE_ELEMENT_ID)?.innerHTML;
+    if (typeof editText === 'string' && editText.length > 0) {
+      postService.addPost(editText);
+    }
+  };
+
+  const getNotes = async () => {
+    const posts = await postService.getPosts();
+    const editerElement = document.getElementById(EDITABLE_ELEMENT_ID);
+    const lastPostContent = posts.at(-1)?.contents;
+    if (editerElement === null || lastPostContent === undefined) return;
+    editerElement.innerHTML = lastPostContent;
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>let's note!</h1>
+        <button onClick={saveNotes}>save</button>
+        <button onClick={getNotes}>get</button>
+      </div>
+
+      <div
+        style={{ height: '100vh', border: '1px solid red' }}
+        contentEditable
+        id={EDITABLE_ELEMENT_ID}
+      ></div>
+    </div>
+  );
+};
+
+export default PostDetail;
